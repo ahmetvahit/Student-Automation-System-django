e2073935e8aceb2ed13d2bba56e66406cb2560b4
Ahmet Vahit Topan <93764056+ahmetvahit@users.noreply.github.com>
2022-03-06 14:10:41 +0300

    Update models.py

--- catalog/deneme/models.py
+++ catalog/deneme/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(max_length=100, verbose_name='Öğrenci Ad')
     surname = models.CharField(max_length=100, verbose_name='Öğrenci Soyad')
     section = models.TextField(verbose_name='Bölüm')
-    number = models.CharField(max_length=50, verbose_name='TC Kimlik No',primary_key=True)
+    number = models.CharField(max_length=50, verbose_name='TC Kimlik No')
     midterm_exam = models.FloatField(verbose_name='Vize Notu',null=True)
     final_exam = models.FloatField(verbose_name='Final Notu',null=True)
     exam_average = models.FloatField( verbose_name='Ortalama Not',null=True)
